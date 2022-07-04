c9ca9c79c47b374dfb6499de56725aebaf9dc97f
artyomchumachenko <artyomchumachenko@mail.ru>
2022-07-04 17:41:54 +0300

    Создание проекта

---
+++ src/main/java/com/example/currencycontrol/CurrencyControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/currencycontrol/CurrencyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
